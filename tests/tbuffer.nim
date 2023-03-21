7377d699d4f8146674cff2d35d754d6ad18604f7
Chancy Kennedy <chancy@conciergecloset.com>
2023-03-21 17:50:49 -0500

    fix tbuffer

--- tests/tbuffer.nim
+++ tests/tbuffer.nim
@@ -1,5 +1,4 @@
 import std/strutils
-import ptr_math
 
 import ../src/buffer
 
@@ -8,6 +7,7 @@ type
     Array = ptr UncheckedArray[char]
 
 block:
+    ## Test a regular read into an UncheckedArray
     var arr = cast[Array](alloc(100 * sizeof(char)))
     var f = open("./tests/sample.txt", fmRead)
     let bytes_read = readBuffer(f, arr, 100)
@@ -16,18 +16,20 @@ block:
     f.close()
 
 block:
+    ## Read into an offset with the UncheckedArray
     var arr = cast[Array](alloc(100 * sizeof(char)))
     arr[0] = 'a'
     arr[1] = 'b'
     var f = open("./tests/sample.txt", fmRead)
-    var arr_ptr = arr[0].addr + 2
+    var arr_ptr = arr[2].addr
     let bytes_read = readBuffer(f, arr_ptr, 98)
     echo "Unchecked Shifted: ", arr.toOpenArray(0, 19).join("")
     assert arr.toOpenArray(0, 19) == "ab111111111111111111"
     f.close()
 
 block:
-    var buffer = allocBuffer(130)
+    ## Test the Buffer API
+    var buffer = allocBuffer(300)
     var f = open("./tests/sample.txt", fmRead)
     var first_bytes_read = f.readIntoBuffer(buffer)
     echo "Total Bytes: ", first_bytes_read
@@ -40,9 +42,3 @@ block:
     assert second_bytes_read == first_bytes_read
     assert buffer.raw[0] == 'a'
     assert buffer.raw[1] == 'b'
-    f.setFilePos(0)
-    buffer = allocBuffer(100)
-    var third_bytes_read = f.readIntoBuffer(buffer)
-    echo "Overflow Bytes: ", third_bytes_Read
-    # readBuffer will not exceed the capacity of the buffer
-    assert third_bytes_read == 100
